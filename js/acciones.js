c10897e7004027f09c082cea6ac7248e49db976d
MARISOL CAMACHO MOTOLINIA <marysolmx08@gmail.com>
2014-08-19 10:28:28 -0500

    BEEP_VIBRAR1
    FUNCIONAL

--- js/acciones.js
+++ js/acciones.js
@@ -2,6 +2,11 @@
 	
 $(document).ready(function(e){	
 	document.addEventListener("deviceready",function(){
-    
+    $('#beep').tap (function() //se utiliza la funcion tap por que en los moviles 
+	{navigator.notification.beep(1);//no existe el click sino tap, arrastre, selección entre otros.
+	});//tap del beep
+	$('#vibrar').tap (function()
+	{navigator.notification.vibrate(1000);
+	});	//tap del vibrate
 	},false); //deviceready
 });//ready
